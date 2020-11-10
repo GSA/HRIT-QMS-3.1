ac2a16418c86db0bf3615168018d8389e7aa7f7e
alfred-ortega <alfred.ortega@gsa.gov>
2020-11-10 15:37:35 -0600

    added explicit byte to bool conversion

--- Qms_Data/UIModel/Notification.cs
+++ Qms_Data/UIModel/Notification.cs
@@ -5,13 +5,17 @@ namespace QmsCore.UIModel
 {
     public class Notification
     {
+        private byte trueByte = 1;
+        private byte falseByte = 0;
+
+
         public int NotificationId { get; set; }
         public int UserId { get; set; }
         public int WorkitemId { get; set; }
         public string WorkItemType { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
-        public byte HasBeenRead { get; set; }
+        public bool HasBeenRead { get; set; }
 
         public DateTime? ReadAt { get; set; }
         public DateTime CreatedAt { get; set; }
@@ -22,7 +26,7 @@ namespace QmsCore.UIModel
 
         public int NotificationEventId {get;set;}
 
-        public byte SendAsEmail {get;set;}
+        public bool SendAsEmail {get;set;}
 
         public User NotificationRecipient {get; private set;}
 
@@ -34,7 +38,8 @@ namespace QmsCore.UIModel
             this.WorkItemType = notification.WorkItemTypeCode;
             this.Title = notification.Title;
             this.Message = notification.Message;
-            this.HasBeenRead = notification.HasBeenRead;
+            this.HasBeenRead = notification.HasBeenRead == 1;
+            this.SendAsEmail = notification.SendAsEmail == 1;
             this.ReadAt = notification.ReadAt;
             this.CreatedAt = notification.CreatedAt;
             this.NotificationRecipient = new User(notification.User,false,false);
@@ -55,12 +60,12 @@ namespace QmsCore.UIModel
             NtfNotification notification = new NtfNotification();
             notification.CreatedAt = this.CreatedAt;
             notification.DeletedAt = this.DeletedAt;
-            notification.HasBeenRead = this.HasBeenRead;
+            notification.HasBeenRead = (this.HasBeenRead) ? trueByte : falseByte;
             notification.Message = this.Message;
             notification.NotificationEventId = this.NotificationEventId;
             notification.NotificationId = this.NotificationId;
             notification.ReadAt = this.ReadAt;
-            notification.SendAsEmail = this.SendAsEmail;
+            notification.SendAsEmail  = (this.SendAsEmail) ? trueByte : falseByte;
             notification.SentAt = this.SentAt;
             notification.Title = this.Title;
             notification.UserId = this.UserId;
