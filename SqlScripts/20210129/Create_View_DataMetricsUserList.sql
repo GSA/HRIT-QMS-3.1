f869aac6f526002a3fb7bf7897575c5b109c2969
alfred-ortega <alfred.ortega@gsa.gov>
2020-12-09 15:39:36 -0600

    Added Create UserList sql

---
+++ SqlScripts/20210129/Create_View_DataMetricsUserList.sql
@@ -0,0 +1,13 @@
+create view aca.qms_datametricsUserList as
+select u.user_id
+      ,u.display_name
+      ,u.email_address
+      ,u.created_at
+      ,r.role_label
+      ,o.org_label
+from aca.sec_user u join aca.sec_user_role ur on u.user_id = ur.user_id
+                    join aca.sec_role r on ur.role_id = r.role_id
+                    join aca.sec_org o on u.orgid = o.org_id
+where u.deleted_at is null
+  and r.deleted_at is null
+order by o.org_id, u.display_name;
